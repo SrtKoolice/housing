8887a13358ecdbd93066b1f842ed29e12cd5f812
unknown <david.silva.lee@gmail.com>
2018-03-29 15:55:45 -0600

    angry with heroku

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -44,7 +44,7 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             'bindings',
-            \App\Http\Middleware\HttpsProtocol::class,
+            //\App\Http\Middleware\HttpsProtocol::class,
         ],
     ];
 
