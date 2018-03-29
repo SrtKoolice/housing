0404992c4e16e6832828f7de8bccdcce1af4f54a
unknown <david.silva.lee@gmail.com>
2018-03-29 13:51:55 -0600

    updated ssl

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -6,10 +6,19 @@ use Closure;
 
 class HttpsProtocol
 {
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
     public function handle($request, Closure $next)
     {
-        if (!$request->secure() && env('APP_ENV') == 'prod') {
+        if (!$request->secure() && env('APP_ENV') === 'production') {
+
             return redirect()->secure($request->getRequestUri());
+
         }
 
         return $next($request);
