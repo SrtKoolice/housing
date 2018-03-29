0ee89076948aa723e0b2fcc5dc248eae8756a1fd
unknown <david.silva.lee@gmail.com>
2018-03-29 13:19:01 -0600

    updated redirect

--- app/Http/Controllers/HomesController.php
+++ app/Http/Controllers/HomesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use App\Home;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+use MongoDB\Driver;
 
 use Illuminate\Routing\Controller as BaseController;
 
